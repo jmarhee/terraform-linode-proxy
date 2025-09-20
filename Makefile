96e53b2a817e4edc64fbe7410bb49e97670ad56a
jdm <jmarhee@interiorae.com>
2025-09-20 14:38:41 -0500

    Updates release script

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+SHELL=/bin/bash
+
 .SILENT:
 tag-release:
 	if [[ $(TAG) == v?.?.? ]]; then echo "Tagging $(TAG)"; elif [[ $(TAG) == v?.?.?? ]]; then echo "Tagging $(TAG)"; else echo "Bad Tag Format: $(TAG)"; exit 1; fi && git tag -a $(TAG) -m "Releasing $(TAG)" ; read -p "Push tag: $(TAG)? " push_tag ; if [ "${push_tag}"="yes" ]; then git push origin $(TAG); fi
--- make-release.sh
+++ make-release.sh
@@ -4,7 +4,15 @@ TAG=$TAG
 TAG_COMMIT=$(git rev-parse --short ${TAG})
 
 AUTH_TOKEN=$GITHUB_TOKEN
-curl \
-  -X POST "https://api.github.com/repos/jmarhee/terraform-linode-proxy/releases" -H "Authorization: token $GITHUB_TOKEN" \
+
+curl -X POST \
+  -H "Authorization: token $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
-  -d "{  \"body\": \"Cutting ${TAG} at ${TAG_COMMIT}\",  \"draft\": false,  \"name\": \"${TAG}\",  \"prerelease\": false,  \"tag_name\": \"${TAG}\" }" 
+  "https://api.github.com/repos/jmarhee/terraform-linode-proxy/releases" \
+  -d "{
+    \"tag_name\": \"$TAG\",
+    \"name\": \"Release $TAG\",
+    \"body\": \"Release notes for $TAG_COMMIT\",
+    \"draft\": false,
+    \"prerelease\": false
+  }"
